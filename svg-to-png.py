3350b0d9eae8d46cbe325c33a97164c54188dd50
ちた <80206622+titaVtuber@users.noreply.github.com>
2021-03-07 23:03:41 +0900

    Add files via upload

---
+++ svg-to-png.py
@@ -0,0 +1,11 @@
+from svglib.svglib import svg2rlg
+from reportlab.graphics import renderPM
+import sys
+import os
+
+args = sys.argv
+filename = args[1]
+filename_without_ext = os.path.splitext(os.path.basename(filename))[0]
+
+drawing = svg2rlg(filename)
+renderPM.drawToFile(drawing, filename_without_ext + ".png", fmt="PNG")
